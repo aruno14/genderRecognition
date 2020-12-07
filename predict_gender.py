c854a406eda1e744cd02dd4ee58242908b53c65c
arnaud <arnaud@outlook.com>
2020-12-07 16:28:22 +0900

    Add predict test file

---
+++ predict_gender.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy
+
+image_size = (48, 48)
+model_name = "gender_model_gray"
+
+model = load_model(model_name)
+for imageName in ["test.jpg"]:
+    print(imageName)
+    test_image = image.load_img(imageName, target_size=image_size, color_mode='grayscale')
+    test_image = image.img_to_array(test_image)
+    test_image = test_image.astype('float32')
+    test_image /= 255.0
+    test_image = numpy.expand_dims(test_image, axis=0)
+    prediction = model.predict(test_image)
+    print(prediction)
